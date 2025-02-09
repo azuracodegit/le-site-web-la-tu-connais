ad9136d36acbdc237a55e865973c695d8d9676c1
Namek Industries <namekindustries@gmail.com>
2025-02-09 16:13:27 +0100

    lll

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,15 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-  base: '/',
+  base: command === 'serve' ? '/' : '/le-site-web-la-tu-connais/',
   build: {
-    outDir: 'dist',
+    outDir: 'docs',
     assetsDir: 'assets',
     emptyOutDir: true,
   },
-});
+}));
